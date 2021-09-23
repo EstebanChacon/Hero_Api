778a86ecd6b8d1fc64378c273eae9ccd71aeb2c6
cbellec <corentin.bellec@etudiant.univ-lr.fr>
2021-09-23 10:34:09 +0200

    data import update

--- herosite/heroapi/management/commands/dataimport.py
+++ herosite/heroapi/management/commands/dataimport.py
@@ -6,3 +6,16 @@ import csv
 class Command(BaseCommand):
     def handle(self, **options):
         print("Importing data ...")
+        CSV_FILE = '../superhero_dataset_full/superheroes_test_dataset.csv'
+        with open(CSV_FILE, newline='', encoding="utf-8") as csvfile:
+            herolist = [{k: v for k, v in row.items()}
+                        for row in csv.DictReader(csvfile, skipinitialspace=True)]
+
+            for item in herolist:
+                hero_params = {
+                    "name": item['name'],
+                    "alias": item['real_name']
+                }
+                hero = Hero(**hero_params)
+                hero.save()
+                print(f"{hero.name} created ...")
