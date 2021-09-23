5f19d82d68a21a31224440d7280e5013037e3ae9
SpeedHeure <esteban.chacon@etudiant.univ-lr.fr>
2021-09-23 10:12:23 +0200

    fix errors and name

--- herosite/heroapi/populate.py
+++ herosite/heroapi/populate.py
@@ -1,12 +1,10 @@
 import csv
 from heroapi.models.py import Hero
 
-CSV_FILE = '../../superhero_dataset_full/superheroes_nlp_dataset.csv'
+CSV_FILE = '../../superhero_dataset_full/superheroes_test_dataset.csv'
 with open(CSV_FILE, newline='', encoding="utf-8") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"' )
     for line in csv_reader:
         print(line[0])
         Hero(line[0], line[1])
         Hero.save()
-
-class co
